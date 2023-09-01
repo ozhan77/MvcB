4a781cb662b32fb3aa33280b0cc1e37e332a247d
oguzhan daskaya <oguzhandky@gmail.com>
2023-09-02 00:00:13 +0300

    İmport admin panel

---
+++ WebApplication1/Controllers/AdminCategoryController.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using ClassLibrary1.Concrete;
+using DataAccessLayer.EntityFramework;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EFCategoryDal());
+        public ActionResult Index()
+        {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator categoryvalidator = new CategoryValidator();
+            ValidationResult result = categoryvalidator.Validate(p);
+            if (result.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
